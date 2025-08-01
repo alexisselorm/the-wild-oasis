d3ac8547731518c438e340dd9d4a6e0b7f194bfb
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-08-01 17:18:29 +0200

    feat: enhance Modal component with context for managing open/close state, improve AddCabin component structure (compound components)

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -2,25 +2,46 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 
 const AddCabin = () => {
-  const [isOpenModel, setIsOpenModel] = useState();
-
   return (
-    <>
-      <Button onClick={() => setIsOpenModel((show) => !show)}>
-        Add new Cabin
-      </Button>
+    <Modal>
+      <Modal.Open opensWindowName="cabin-form">
+        <Button>Add new cabins</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
 
-      {isOpenModel && (
-        <Modal onClose={() => setIsOpenModel((open) => !open)}>
-          <CreateCabinForm
-            onCloseModal={() => setIsOpenModel((open) => !open)}
-          />
-        </Modal>
-      )}
-    </>
+      <Modal.Open opensWindowName="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable/>
+      </Modal.Window>
+    </Modal>
   );
 };
+
+// const AddCabin = () => {
+//   const [isOpenModel, setIsOpenModel] = useState();
+
+//   return (
+//     <>
+//       <Button onClick={() => setIsOpenModel((show) => !show)}>
+//         Add new Cabin
+//       </Button>
+
+//       {isOpenModel && (
+//         <Modal onClose={() => setIsOpenModel((open) => !open)}>
+//           <CreateCabinForm
+//             onCloseModal={() => setIsOpenModel((open) => !open)}
+//           />
+//         </Modal>
+//       )}
+//     </>
+//   );
+// };
 
 export default AddCabin;
