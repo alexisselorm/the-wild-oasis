48694db72eca199941c8e25f30fbb99cc29f8e4b
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2026-03-11 00:06:07 +0100

    Refactor imports in UpdateUserDataForm and clean up logout function in apiAuth

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Recent activity</div>
+      <div>Chart Stay duration</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
